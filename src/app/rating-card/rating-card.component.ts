7af1b23fd207de81c29614ab2d5e3563aa3b0c3d
Tagline <tagline@Taglines-iMac-Pro.local>
2022-12-24 17:06:27 +0530

    Changes in rating phase

--- src/app/rating-card/rating-card.component.ts
+++ src/app/rating-card/rating-card.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+interface Revboxval {
+  number: string;
+  review: string;
+  name: string;
+  verify: string;
+}
 
 @Component({
   selector: 'app-rating-card',
   templateUrl: './rating-card.component.html',
-  styleUrls: ['./rating-card.component.scss']
+  styleUrls: ['./rating-card.component.scss'],
 })
 export class RatingCardComponent implements OnInit {
+  @Input() revBox!: Revboxval[];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
